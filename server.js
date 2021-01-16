a47b0219f1470154c3932920f74f1c329267b779
Pratik Solanki <45298146+pratikksolanki@users.noreply.github.com>
2021-01-16 12:56:38 -0500

    inline js

--- server.js
+++ server.js
@@ -61,5 +61,5 @@ function generateToken(roomName, response) {
 }
 
 const listener = app.listen(process.env.PORT, () => {
-  console.log("Your app is listening on port " + listener.address().port);
-});
+    console.log("Your app is listening on port " + listener.address().port);
+  });
