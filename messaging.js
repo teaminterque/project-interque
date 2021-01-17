2a35dd3733421fd9feb0eb23c1c74952bb55b212
Harsh Dave <hdave009@users.noreply.github.com>
2021-01-16 20:14:08 -0500

    Update messaging.js

--- messaging.js
+++ messaging.js
@@ -6,14 +6,13 @@ var time = new Date();
 
 const database = firebase.database();
 
-
 sendButton.addEventListener('click', (e) => {
   e.preventDefault();
   if(nameEntry.value == null || textEntry.value == null){
     alert("Please enter your name and a message.");
   } else {
     database.ref('/' + "messages").update({
-      [nameEntry.value + time.getTime()] : textEntry.value
+      [1 + nameEntry.value] : textEntry.value
     });
   }
   textEntry.value = null;
