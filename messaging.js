579ae778ec6e1e8bed2564988ae3fb851c0c0ad0
Harsh Dave <hdave009@users.noreply.github.com>
2021-01-16 20:44:57 -0500

    Update messaging.js

--- messaging.js
+++ messaging.js
@@ -11,9 +11,17 @@ sendButton.addEventListener('click', (e) => {
   if(nameEntry.value == null || textEntry.value == null){
     alert("Please enter your name and a message.");
   } else {
-    database.ref('/' + "messages").update({
-      [1 + nameEntry.value] : textEntry.value
+
+    var msgNum = 0; 
+    database.ref('/Count').on('value', (snapshot) => {
+      msgNum = snapshot.val();
     });
+
+    database.ref('/' + "messages").update({
+      [(msgNum + 1) + nameEntry.value] : textEntry.value
+    });
+    
+    database.ref('/Count').set(msgNum + 1);
   }
   textEntry.value = null;
 })
